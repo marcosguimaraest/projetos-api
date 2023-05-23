827f4336fb61301f440c4539dc84a7cdd52f1357
Marcola <marcosguimaraest04@gmail.com>
2023-05-23 13:41:42 -0300

    Criado arquivo data.sql para carregar dados

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+use projetosapi;
+INSERT INTO tb_projetos VALUES (1, 'VallyTool', "Projeto na FAETEC", "https://github.com/marcosguimaraest/VallyTool");
+INSERT INTO tb_projetos VALUES (2, 'Projeto RPG', "Projeto de uma plataforma para joggar RPG com amigos. Em desenvolvimento", "https://github.com/marcosguimaraest/projeto-rpg");
