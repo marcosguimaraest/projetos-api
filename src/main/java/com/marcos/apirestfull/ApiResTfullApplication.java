989afdff7adeb6dfc7bc1d6dd6c5bf062a537207
Marcola <marcosguimaraest04@gmail.com>
2023-05-23 09:37:03 -0300

    Projeto Inicial

---
+++ src/main/java/com/marcos/apirestfull/ApiResTfullApplication.java
@@ -0,0 +1,13 @@
+package com.marcos.apirestfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiResTfullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiResTfullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcos/apirestfull/ApiResTfullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.apirestfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiResTfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
